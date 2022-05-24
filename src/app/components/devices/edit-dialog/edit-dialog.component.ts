e177542bd84ec97c3c2e112812ad9fe0a25b169f
David Aguilar Martín <david.aguilar.cdc@gmail.com>
2022-05-24 15:15:19 +0200

    Edit dialog form

--- src/app/components/devices/edit-dialog/edit-dialog.component.ts
+++ src/app/components/devices/edit-dialog/edit-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { DevicesData } from 'src/app/models/device';
 
@@ -8,6 +9,13 @@ import { DevicesData } from 'src/app/models/device';
   styleUrls: ['./edit-dialog.component.css']
 })
 export class EditDialogComponent implements OnInit {
+
+  public BrandControl = new FormControl('', Validators.required);
+  public ModelControl = new FormControl('', Validators.required);
+  public SerialNumbControl = new FormControl('', Validators.required);
+  public OSControl = new FormControl('', Validators.required);
+  public VersionControl = new FormControl('', Validators.required);
+  public brandSmartphone = 'null';
 
   constructor(public dialogRef: MatDialogRef<EditDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DevicesData) { }
