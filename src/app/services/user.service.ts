ff7a4dc49dcbf596d0a8684eef33616da2bce401
Victor Monzo Mora <victor.monzo.mora@gmail.com>
2022-05-26 17:05:50 +0200

    User service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { UserData } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+
+  saveUser(user: UserData) {
+    localStorage.setItem('logged', '1');
+    localStorage.setItem('id', user.id.toString());
+    localStorage.setItem('firstName', user.firstName.toString());
+    localStorage.setItem('lastName', user.lastName.toString());
+    localStorage.setItem('email', user.email.toString());
+  }
+
+  getUser(): UserData {
+    return {
+      id: Number(localStorage.getItem('id')!),
+      email: localStorage.getItem('email')!,
+      firstName: localStorage.getItem('firstName')!,
+      lastName: localStorage.getItem('lastName')!,
+      password: ''
+    };
+  }
+
+  getId(): Number {
+    return Number(localStorage.getItem('id')!);
+  }
+
+  clearUser() {
+    localStorage.clear();
+  }
+}
