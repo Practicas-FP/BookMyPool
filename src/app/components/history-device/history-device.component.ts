0e0836ea9a19da09c293f78b830b3866cbc43623
David Aguilar Martín <david.aguilar.cdc@gmail.com>
2022-05-27 12:10:25 +0200

    History Device table

--- src/app/components/history-device/history-device.component.ts
+++ src/app/components/history-device/history-device.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-history-device',
   templateUrl: './history-device.component.html',
   styleUrls: ['./history-device.component.css']
 })
-export class HistoryDeviceComponent implements OnInit {
+export class HistoryDeviceComponent implements AfterViewInit {
+  displayedColumns: string[] = ['position', 'employee', 'lendingDate', 'returningDate'];
+  dataSource = new MatTableDataSource<deviceHistory>(ELEMENT_DATA);
 
-  constructor() { }
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
-
 }
+
+export interface deviceHistory {
+  employee: string;
+  position: number;
+  lendingDate: string;
+  returningDate: string;
+}
+
+const ELEMENT_DATA: deviceHistory[] = [
+  {position: 1, employee: 'David Aguilar', lendingDate: '15-04-22', returningDate: '22-05-22'},
+  {position: 1, employee: 'Victor Monzó', lendingDate: '15-04-22', returningDate: '22-05-22'},
+  {position: 1, employee: 'Alvaro Albarracín', lendingDate: '15-04-22', returningDate: '22-05-22'},
+  {position: 1, employee: 'Nico Esteban', lendingDate: '15-04-22', returningDate: '22-05-22'},
+  {position: 1, employee: 'Jose Hernández', lendingDate: '15-04-22', returningDate: '22-05-22'},
+  {position: 1, employee: 'Alvaro Carrillo', lendingDate: '15-04-22', returningDate: '22-05-22'},
+];
