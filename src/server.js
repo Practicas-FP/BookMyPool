31bc0b25d314e48fa3b46a26f39f2f41bcccb09f
jose-hr14 <71669454+jose-hr14@users.noreply.github.com>
2022-05-30 13:17:35 +0200

    make it better

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/<book-my-pool>'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/<book-my-pool>/index.html'));});
+
+app.use(express.static(__dirname+'/dist/ng-blog'));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+});
+
 app.listen(process.env.PORT || 8080);
